f768b28c7dfc4f02fb5a4ed0824ffe9eb06fa795
E. Pineapple <e.pineapple@pineapple.st>
2025-07-25 06:48:28 -0500

    chore: update dependencies
    Update dependencies.  Update esLint configuration to work with the
    new dependencies.  Use type information while linting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,24 @@
 // @ts-check
 
+import { globalIgnores } from "eslint/config";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  globalIgnores(["dist/"]),
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  { ignores: ["dist/**"] },
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ["eslint.config.mjs", "lint-staged.config.mjs", "vite.config.ts"],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
 );
